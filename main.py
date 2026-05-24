3a8abf21972a993b0f5df28284eab154fcb069a7
123idon <108921068+123idon@users.noreply.github.com>
2026-05-24 21:47:31 +0900

    fix: 중복기준 완화 date+author+content+product

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def _load_reviews_cached():
         jasaol_deduped = []
         for rv in jasaol_base + jasaol_new:
             # (날짜+작성자+내용) 기준 중복제거
-            key = (rv.get("date",""), rv.get("author",""), rv.get("content","")[:80])
+            key = (rv.get("date",""), rv.get("author",""), rv.get("content","")[:80], rv.get("product","")[:40])
             if key not in seen_keys:
                 seen_keys.add(key)
                 jasaol_deduped.append(rv)
